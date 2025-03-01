f8b83435d1a4bc7fa26b49fec0a49b2c4a63b98c
Rahul Kumar Singh <rs3448397@gmail.com>
2025-03-01 13:22:27 +0530

    Updated bit manipulation logic

--- medium/bitManipulation.cpp
+++ medium/bitManipulation.cpp
@@ -10,8 +10,17 @@ void checkEvenOdd(int num){
     }
 }
 
-int main (){
-    checkEvenOdd(6);
-    checkEvenOdd(7);
+void setBit( int &num, int pos){
+    num |= (1<<pos);
+}
 
+int main (){
+    // checkEvenOdd(6);
+    // checkEvenOdd(7);
+    int num = 5;
+
+    setBit(num, 1);
+    setBit(num, 2);
+    cout<<num<<endl;
+    return 0;
 }
